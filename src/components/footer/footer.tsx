09176e7342da9b61158061f8119a3a5f3041a797
LoboReflexivo <darioguerragarcia.lr@gmail.com>
2024-05-03 10:14:26 +0200

    completed

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Styles from "./footer.module.css";
 
 function FooterComponent() {
-  return <footer className={Styles.footer}>&copy;Dario Guerra García</footer>;
+  return <footer className={Styles.footer}>&copy;Darío Guerra García</footer>;
 }
 
 export default FooterComponent;
